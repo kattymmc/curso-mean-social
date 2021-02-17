cb5cf34c83ae8548796d2b3c3a66b0f10afa44bc
Katty <katherine.miraval@unmsm.edu.pe>
2021-02-17 16:38:59 -0500

    puerto añadido

--- API/index.js
+++ API/index.js
@@ -1,11 +1,18 @@
 'use strict'
 
 var mongoose = require('mongoose');
+var app = require('./app')
+var port = 3800;
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/curso-mean-social', 
         { useNewUrlParser: true, useUnifiedTopology: true })
         .then(() => {
             console.log("Se conectó a curso-mean-social")
+
+            app.listen(port, ()=>{
+                console.log(`Servidor corriendo en http://localhost:/${port}`);
+            });
+            
         })
         .catch(err => console.log(err))
