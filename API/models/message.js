3f5f6407057ed2bd1a3ac7298541177d3753c632
Katty <katherine.miraval@unmsm.edu.pe>
2021-02-17 17:20:00 -0500

    modelo message CREADO

---
+++ API/models/message.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+
+let MessageSchema = new Schema({
+    text: { type: String },
+    create_at: { type: String },
+    emmiter: { type: Schema.ObjectId, ref: 'User' },
+    receiver: { type: Schema.ObjectId, ref: 'User' }
+});
+
+module.exports = mongoose.model('Message', MessageSchema);
