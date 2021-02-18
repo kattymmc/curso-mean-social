17306afcd04d7b87cb0b331c1dffbc4f03934c4e
Katty <katherine.miraval@unmsm.edu.pe>
2021-02-18 12:21:57 -0500

    Login de usuarios

--- API/controllers/user-controller.js
+++ API/controllers/user-controller.js
@@ -29,21 +29,36 @@ function saveUser(req, res){
            user.role = 'ROLE_USER';
            user.image = null;
 
-           // Encriptar contraseña
-           bcrypt.hash(params.password, null, null, (err, hash) => {
-               user.password = hash;
-               // Guardar usuario
-               user.save((err, userStored) => {
-                   if(err) return res.status(500).send({
-                       message: 'Error al guardar el usuario'
-                   });
-                   if(userStored){
-                       res.status(200).send({user: userStored});
-                   } else {
-                       res.status(404).send({message: 'No se ha registrao al usuario'})
-                   }
-               });
-           });
+           // CONTROLAR USUARIOS DUPLICADOS
+            User.find({ $or: [
+                {email: user.email.toLowerCase()},
+                {nick: user.nick.toLowerCase()}
+            ]}).exec((err, users) => {
+                if(err) return res.status(500).send({ message: 'Error en la petición de usuarios' });
+
+                if(users && users.length >= 1){
+                    return res.status(200).send({ message: 'El usuario ya esta registrado' });
+                } else {
+
+                    // ENCRIPTAR CONTRASEÑA
+                    bcrypt.hash(params.password, null, null, (err, hash) => {
+                        user.password = hash;
+
+                        // GUARDAR USUARIO
+                        user.save((err, userStored) => {
+                            if(err) return res.status(500).send({
+                                message: 'Error al guardar el usuario'
+                            });
+                            if(userStored){
+                                res.status(200).send({user: userStored});
+                            } else {
+                                res.status(404).send({message: 'No se ha registrao al usuario'})
+                            }
+                        });
+                    });
+                }
+            });
+
        } else {
            res.status(200).send({
                message: 'Faltan campos necesarios'
@@ -51,8 +66,31 @@ function saveUser(req, res){
        }
 }
 
+function loginUser(req, res){
+    var params = req.body;
+    var email = params.email;
+    var password = params.password;
+
+    User.findOne({email: email}, (err, user) => {
+        if(err) return res.status(500).send({message: 'Error en la peticion'});
+
+        if(user){
+            bcrypt.compare(password, user.password, (err,check) => {
+                if(check){
+                    // Devolver datos del usuario
+                    return res.status(200).send({user});
+                }
+                    return res.status(404).send({message: 'El usuario no se ha podido identificar'});
+            });
+        } else {
+            return res.status(404).send({message: 'El usuario no se ha podido identificar'});
+        }
+    });
+}
+
 module.exports = {
     home,
     pruebas,
-    saveUser
+    saveUser,
+    loginUser
 }
