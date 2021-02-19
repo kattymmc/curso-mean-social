37fe55cc58797ef97cd1992ff93b22b93e99a521
Katty <katherine.miraval@unmsm.edu.pe>
2021-02-19 16:28:15 -0500

    metodo de autenticacion creado

---
+++ API/middlewares/authenticated.js
@@ -0,0 +1,31 @@
+'use strict'
+
+var jwt = require('jwt-simple');
+var moment = require('moment');
+var secret = 'clave_secreta_del_proyecto_chinchin';
+
+exports.ensureAuth = function(req, res, next){
+    if(!req.headers.authorization){
+        return res.status(403).send({message: 'La peticion no tiene autenticacion'})
+    }
+    // Eliminar las comillas dobles y simples
+    var token = req.headers.authorization.replace(/['"]+/g,'');
+
+    try{
+        var payload = jwt.decode(token, secret);
+        // Si ya pasó el tiempo de expiración
+        if(payload.exp <= moment().unix()){
+            return res.status(401).send({
+                message: 'El token ha expirado'
+            });
+        }
+    } catch(ex){
+        return res.status(404).send({
+            message: 'El token no es valido'
+        });
+    }
+    req.user = payload;
+
+    next();
+
+}
