61aa97224460dbd0e9b9e3915485b1a3503e75c3
Katty <katherine.miraval@unmsm.edu.pe>
2021-02-20 00:34:16 -0500

    Eliminacion de follows

--- API/routes/follow-routes.js
+++ API/routes/follow-routes.js
@@ -8,5 +8,6 @@ var md_auth = require('../middlewares/authenticated');
 
 api.get('/pruebas-follow', md_auth.ensureAuth, FollowController.pruebas);
 api.post('/follow', md_auth.ensureAuth, FollowController.saveFollow);
+api.delete('/follow/:id', md_auth.ensureAuth, FollowController.deleteFollow);
 
 module.exports = api;
