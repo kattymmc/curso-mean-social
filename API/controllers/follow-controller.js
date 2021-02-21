c2c863941a337039c5bd619671298143a02564c9
Katty <katherine.miraval@unmsm.edu.pe>
2021-02-20 23:38:10 -0500

    Paginación de followed

--- API/controllers/follow-controller.js
+++ API/controllers/follow-controller.js
@@ -71,9 +71,41 @@ function getFollowingUsers(req, res){
     });
 }
 
+function getFollowedUsers(req, res){
+    var userId = req.user.sub;
+
+    if(req.params.id && req.params.page){
+        userId = req.params.id;
+    }
+
+    var page = 1;
+
+    if(req.params.page){
+        page = req.params.page;
+    } else {
+        page = req.params.id;
+    }
+
+    var itemsPerPage = 4;
+
+    // Populate: te devuelve todo el objeto de user con sus datos 
+    Follow.find({followed: userId}).populate({path: 'user'}).paginate(page, itemsPerPage, (err, follows, total) => {
+        if(err) return res.status(500).send({message: 'Error en el servidor'});
+
+        if(!follows) return res.status(404).send({message: 'No te sigue ningun usuario'});
+
+        return res.status(200).send({
+            total: total,
+            pages: Math.ceil(total/itemsPerPage),
+            follows
+        });
+    });
+}
+
 module.exports = {
     pruebas,
     saveFollow,
     deleteFollow,
     getFollowingUsers,
+    getFollowedUsers
 }
