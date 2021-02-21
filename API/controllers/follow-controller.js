34086d9b249eb462f3c673c5318aa3af101da95b
Katty <katherine.miraval@unmsm.edu.pe>
2021-02-20 23:36:51 -0500

    Paginación de followings

--- API/controllers/follow-controller.js
+++ API/controllers/follow-controller.js
@@ -40,8 +40,40 @@ function deleteFollow(req, res){
     });
 }
 
+function getFollowingUsers(req, res){
+    var userId = req.user.sub;
+
+    if(req.params.id && req.params.page){
+        userId = req.params.id;
+    }
+
+    var page = 1;
+
+    if(req.params.page){
+        page = req.params.page;
+    } else {
+        page = req.params.id;
+    }
+
+    var itemsPerPage = 4;
+
+    // Populate: te devuelve todo el objeto de usuario con sus datos 
+    Follow.find({user: userId}).populate({path: 'followed'}).paginate(page, itemsPerPage, (err, follows, total) => {
+        if(err) return res.status(500).send({message: 'Error en el servidor'});
+
+        if(!follows) return res.status(404).send({message: 'No estas siguiendo a ningun usuario'});
+
+        return res.status(200).send({
+            total: total,
+            pages: Math.ceil(total/itemsPerPage),
+            follows
+        });
+    });
+}
+
 module.exports = {
     pruebas,
     saveFollow,
-    deleteFollow
+    deleteFollow,
+    getFollowingUsers,
 }
--- API/routes/follow-routes.js
+++ API/routes/follow-routes.js
@@ -9,5 +9,6 @@ var md_auth = require('../middlewares/authenticated');
 api.get('/pruebas-follow', md_auth.ensureAuth, FollowController.pruebas);
 api.post('/follow', md_auth.ensureAuth, FollowController.saveFollow);
 api.delete('/follow/:id', md_auth.ensureAuth, FollowController.deleteFollow);
+api.get('/following/:id?/:page?', md_auth.ensureAuth, FollowController.getFollowingUsers);
 
 module.exports = api;
