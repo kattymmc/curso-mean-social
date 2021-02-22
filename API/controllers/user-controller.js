a6f724097226a1209f27e9bfa45cc62bf1d5ed9e
Katty <katherine.miraval@unmsm.edu.pe>
2021-02-22 17:16:05 -0500

    Comprobar si nos seguimos mutuamente - añadido

--- API/controllers/user-controller.js
+++ API/controllers/user-controller.js
@@ -112,12 +112,38 @@ function getUser(req, res){
         
         if(!user) return res.status(404).send({message: 'El usuario no existe'});
         
-        Follow.findOne({"user": req.user.sub, "followed": userId}).exec((err, follow) => {
-            if(err) return res.status(500).send({message: 'Error al comprobar el seguimiento'});
-        
-            return res.status(200).send({user, follow});
+        followThisUser(req.user.sub, userId).then((value) => {
+            return res.status(200).send({
+                user, 
+                following: value.following,
+                followed: value.followed
+            });
         });
     });
+}
+
+async function followThisUser(indentity_user_id, user_id){
+    try{
+        var following = await Follow.findOne({"user":indentity_user_id, "followed":user_id}).exec()
+                        .then((following) => {
+                            return following;
+                        }).catch((err) => {
+                            return handleerror(err);
+                        })
+        
+        var followed = await Follow.findOne({"user":user_id, "followed":indentity_user_id}).exec()
+                        .then((followed) => {
+                            return followed;
+                        }).catch((err) => {
+                            return handleerror(err);
+                        })
+        return {
+            following: following,
+            followed: followed
+        };
+    }catch(e){
+        console.log(e);
+    }
 }
 
 // PEDIR DATOS DE USUARIOS PAGINADO
