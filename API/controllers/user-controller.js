a4673b8feee439955fa5c971a38b172f51f3e44d
Katty <katherine.miraval@unmsm.edu.pe>
2021-02-22 12:48:35 -0500

    Comprobar si sigo a alguien añadido

--- API/controllers/user-controller.js
+++ API/controllers/user-controller.js
@@ -6,6 +6,8 @@ var path = require('path');
 
 var jwt = require('../services/jwt');
 var User = require('../models/user');
+var Follow = require('../models/follow');
+
 
 // Rutas
 function home(req, res){
@@ -110,8 +112,12 @@ function getUser(req, res){
         
         if(!user) return res.status(404).send({message: 'El usuario no existe'});
         
-        return res.status(200).send({user});
-    })
+        Follow.findOne({"user": req.user.sub, "followed": userId}).exec((err, follow) => {
+            if(err) return res.status(500).send({message: 'Error al comprobar el seguimiento'});
+        
+            return res.status(200).send({user, follow});
+        });
+    });
 }
 
 // PEDIR DATOS DE USUARIOS PAGINADO
