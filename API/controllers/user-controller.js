bac96ed768a8e9eb06f16b86f2917dbc80e275dd
Katty <katherine.miraval@unmsm.edu.pe>
2021-02-22 18:39:47 -0500

    Mostrar array con usuarios seguidos y que nos siguen

--- API/controllers/user-controller.js
+++ API/controllers/user-controller.js
@@ -161,12 +161,50 @@ function getUsers(req, res){
 
         if(!users) return res.status(404).send({message: 'No hay usuarios disponibles'});
 
-        return res.status(200).send({
-            users,
-            total,
-            pages: Math.ceil(total/itemsPerPage)
+        followUserIds(identity_user_id).then((value) => {
+            return res.status(200).send({
+                users,
+                users_following: value.following,
+                users_follow_me: value.followed,
+                total,
+                pages: Math.ceil(total/itemsPerPage)
+            });
         });
     });
+}
+
+async function followUserIds(user_id){
+    var following = await Follow.find({"user": user_id}).select({'_id':0, '_v':0, 'user':0})
+                    .exec().then((follows) => {
+                        var follows_clean = [];
+                        follows.forEach((follow) => {
+                            follows_clean.push(follow.followed);
+                        });
+                        return follows_clean;
+                    }).catch((e) =>{
+                        return handleerror(err);
+                    });
+    var followed = await Follow.find({"followed": user_id}).select({'_id':0, '_v':0, 'followed':0})
+                    .exec().then((follows) => {
+                        var follows_clean = [];
+                        follows.forEach((follow) => {
+                            follows_clean.push(follow.user);
+                        });
+                        return follows_clean;
+                    }).catch((e) =>{
+                        return handleerror(err);
+                    });
+
+
+    /*var followed_clean = [];
+    followed.forEach((follow) => {
+        followed_clean.push(follow.user);
+    });*/
+
+    return{
+        following: following,
+        followed: followed
+    }
 }
 
 // EDICION DE DATOS DE USUARIO
