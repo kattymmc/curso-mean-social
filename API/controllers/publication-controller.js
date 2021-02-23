7a8d383c93d8095d687644e55c3fbcb16cc881d6
Katty <katherine.miraval@unmsm.edu.pe>
2021-02-23 00:55:54 -0500

    Registro de publicaciones

--- API/controllers/publication-controller.js
+++ API/controllers/publication-controller.js
@@ -14,6 +14,27 @@ function probando(req, res){
     });
 }
 
+function savePublication(req, res){
+    var params = req.body;
+
+    if(!params.text) return res.status(200).send({message: 'Debes enviar un texto'});
+
+    var publication = new Publication();
+    publication.text = params.text;
+    publication.file = 'null';
+    publication.user = req.user.sub;
+    publication.created_at = moment().unix();
+
+    publication.save((err, publicationStored) => {
+        if(err) return res.status(500).send({message: 'Error al guardar la publicacion'});
+
+        if(!publicationStored) return res.status(404).send({message: 'La publicacion NO ha sido guardada'});
+
+        return res.status(200).send({publication: publicationStored});
+    });
+}
+
 module.exports = {
-    probando
+    probando,
+    savePublication
 }
