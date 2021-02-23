ee20997e305fa9f91f92d6e4eda95247d844910d
Katty <katherine.miraval@unmsm.edu.pe>
2021-02-22 21:28:02 -0500

    publication-routes CREADO

--- API/app.js
+++ API/app.js
@@ -8,6 +8,7 @@ var app = express();
 // Cargar rutas
 var userRoutes = require('./routes/user-routes');
 var followRoutes = require('./routes/follow-routes');
+var publicationRoutes = require('./routes/publication-routes');
 
 //  Middlewares
 app.use(bodyParser.urlencoded({extended: false}));
@@ -19,5 +20,6 @@ app.use(bodyParser.json());
 // Rutas -> http://localhost:3800/api/ruta
 app.use('/api', userRoutes);
 app.use('/api', followRoutes);
+app.use('/api', publicationRoutes);
 
 module.exports = app;
