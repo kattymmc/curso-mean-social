796746bbeb5774f2d0aeedb1cb556214817aa907
Katty <katherine.miraval@unmsm.edu.pe>
2021-02-24 15:17:52 -0500

    Modelo message modificado

--- API/models/message.js
+++ API/models/message.js
@@ -5,6 +5,7 @@ let Schema = mongoose.Schema;
 
 let MessageSchema = new Schema({
     text: { type: String },
+    viewed: { type: String },
     create_at: { type: String },
     emmiter: { type: Schema.ObjectId, ref: 'User' },
     receiver: { type: Schema.ObjectId, ref: 'User' }
