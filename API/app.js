649fa592c986f2580614e96b5fa1d4b2a033d3cb
Katty <katherine.miraval@unmsm.edu.pe>
2021-02-25 00:23:22 -0500

    CONFIGURACION DE CABECERA PARA PETICIONES AJAX

--- API/app.js
+++ API/app.js
@@ -16,6 +16,15 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 // Cors
+// configuracion de cabeceras para aceptar las peticiones AJAX desde cualquier origen
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+ 
+    next();
+});
 
 
 // Rutas -> http://localhost:3800/api/ruta
