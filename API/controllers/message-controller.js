5980126d75595b41adb451f98c67ee1e9aa74c69
Katty <katherine.miraval@unmsm.edu.pe>
2021-02-24 22:53:49 -0500

    Metodo obtener mensajes enviados

--- API/controllers/message-controller.js
+++ API/controllers/message-controller.js
@@ -55,8 +55,30 @@ function getReceivedMessages(req, res){
     })
 }
 
+function getEmmitMessages(req, res){
+    var userId = req.user.sub;
+
+    var page = 1;
+    if(req.params.page){
+        page = req.params.page;
+    }
+    var itemsPerPage = 4;
+    // Populate: el segundo parametro indica los campos que se quiere mostrar en la petición
+    Message.find({emitter: userId}).populate('emitter receiver', 'name surname image nick _id').paginate(page, itemsPerPage, (err, messages, total) => {
+        if(err) return res.status(500).send({message: 'Error en la peticion'});
+        if(!messages) return res.status(404).send({message: 'No hay mensajes'});
+
+        return res.status(200).send({
+            total:total,
+            pages: Math.ceil(total/itemsPerPage),
+            messages
+        })
+    })
+}
+
 module.exports ={
     probando,
     saveMessage,
-    getReceivedMessages
+    getReceivedMessages,
+    getEmmitMessages
 }
